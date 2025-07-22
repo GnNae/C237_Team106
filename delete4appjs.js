bc90b52f161d066be893a58b93b6aa9998534c45
LimKaiXin <24002707@myrp.edu.sg>
2025-07-22 21:43:38 +0800

    Create delete4appjs.js
    Delete code for app.js

---
+++ delete4appjs.js
@@ -0,0 +1,14 @@
+app.get('/deleteProduct/:id', (req, res) => { 
+    const productId = req.params.id; 
+    connection.query('DELETE FROM products WHERE petId = ?', [petId], (error, results) => { 
+
+        if (error) { 
+            // Handle any error that occurs during the database operation 
+            console.error("Error deleting product:", error); 
+            res.status(500).send('Error deleting product'); 
+        } else { 
+            // Send a success response 
+            res.redirect('/inventory'); 
+        } 
+    }); 
+}); 
